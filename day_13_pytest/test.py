126449744d9af100c0111345b26ac57971ca2256
ATTA UR REHMAN <attaur5565@gmail.com>
2026-06-07 17:39:59 +0500

    Add FastAPI application and initial test cases for endpoints

---
+++ day_13_pytest/test.py
@@ -0,0 +1,15 @@
+from fastapi.testclient import TestClient
+from main import app
+
+
+client = TestClient(app)
+
+def test_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World!"}
+
+def test_home():
+    response = client.get("/home")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Welcome to the home page!"}
