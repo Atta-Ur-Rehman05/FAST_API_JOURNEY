6c5862af8e744c695b25d5778ca2f111206a7383
ATTA UR REHMAN <attaur5565@gmail.com>
2026-08-01 21:37:03 +0500

    chore: add .gitignore file to exclude environment, cache, and log files

--- E-commerce-api/tests/test_main.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from httpx import AsyncClient
-
-@pytest.mark.asyncio
-async def test_read_main(client: AsyncClient):
-    response = await client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"message": "Welcome to the E-commerce API"}
-
